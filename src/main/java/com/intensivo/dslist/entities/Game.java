364b0639e004d80fc10b19e59ddc2560a3481a4b
IfDevfernando <123130367+IfDevfernando@users.noreply.github.com>
2023-11-15 14:51:46 -0300

    game insert

--- src/main/java/com/intensivo/dslist/entities/Game.java
+++ src/main/java/com/intensivo/dslist/entities/Game.java
@@ -21,9 +21,16 @@ public class Game {
 	@Column(name="game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 
@@ -32,16 +39,18 @@ public class Game {
 	}
 
 
-	public Game(long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 	
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score=score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
+	
 		this.longDescription = longDescription;
 	}
 
@@ -86,13 +95,22 @@ public class Game {
 	}
 
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+	public Double getScore() {
+		return score;
+	}
+
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 
@@ -143,6 +161,9 @@ public class Game {
 		Game other = (Game) obj;
 		return id == other.id;
 	}
+
+
+	
 	
 
 }
