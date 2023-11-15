114967917a517b7caf97f07d468d3da398eafa28
IfDevfernando <123130367+IfDevfernando@users.noreply.github.com>
2023-11-15 19:36:35 -0300

    get id

--- src/main/java/com/intensivo/dslist/controller/GameController.java
+++ src/main/java/com/intensivo/dslist/controller/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.intensivo.dslist.dto.GameDTO;
 import com.intensivo.dslist.dto.GameMinDto;
 import com.intensivo.dslist.service.GameService;
 
@@ -21,6 +23,11 @@ public class GameController {
 	@GetMapping
 	public List<GameMinDto> findAll(){
 		List<GameMinDto> result=gameService.findAll();
+		return result;
+	}
+	@GetMapping(value="/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result=gameService.findById(id);
 		return result;
 	}
 	
