a9405781122b5e5a9f82061c3cf9a9b635408622
IfDevfernando <123130367+IfDevfernando@users.noreply.github.com>
2023-11-16 19:33:43 -0300

    omolog ok

--- src/main/java/com/intensivo/dslist/dto/GameMinDto.java
+++ src/main/java/com/intensivo/dslist/dto/GameMinDto.java
@@ -27,7 +27,7 @@ public class GameMinDto {
 		
 		id = projection.getId();
 		title = projection.getTitle();
-		year = projection.getYear();
+		year = projection.getGameYear();
 		imgUrl = projection.getImgUrl();
 		shortDescription = projection.getShortDescription();
 	}
